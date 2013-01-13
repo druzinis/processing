85b5cd57da6821e3477e5680bc6309339e0304be
driu <andrius.druzinis@gmail.com>
2013-01-13 16:33:36 +0000

    Error fixed

--- burton/code - plurals removed.R
+++ burton/code - plurals removed.R
@@ -1,18 +1,24 @@
 words <- read.csv("burton.csv", stringsAsFactors=FALSE)[, 2:3]
 words <- words[order(words$zodis), ]
 
-ids <- which(words$zodis[-length(words$zodis)]==gsub("s$", "", words$zodis[-1]))
-words$total <- words$skaicius
-words$total[ids] <- rowSums(cbind(words$skaicius[ids], words$skaicius[ids+1]))
-words$singular <- ""
-words$singular[ids] <- words$zodis[ids]
-words$singular.num <- 0
-words$singular.num[ids] <- words$skaicius[ids]
-words$plural <- ""
-words$plural[ids] <- words$zodis[ids + 1]
-words$plural.num <- 0
-words$plural.num[ids] <- words$skaicius[ids+1]
+# ids <- which(words$zodis[-length(words$zodis)]==gsub("s$", "", words$zodis[-1]))
 
-output <- words[-(ids+1), -2]
+freq <- as.data.frame(table(gsub("s$", "", words$zodis)))
+singulars <- as.character(freq$Var1[freq$Freq>1])
+sing.ids <- sapply(singulars, function(i) which(i==words$zodis))
+plur.ids <- sapply(paste0(singulars, "s"), function(i) which(i==words$zodis))
+
+words$total <- words$skaicius
+words$total[sing.ids] <- rowSums(cbind(words$skaicius[sing.ids], words$skaicius[plur.ids]))
+words$singular <- ""
+words$singular[sing.ids] <- words$zodis[sing.ids]
+words$singular.num <- 0
+words$singular.num[sing.ids] <- words$skaicius[sing.ids]
+words$plural <- ""
+words$plural[sing.ids] <- words$zodis[plur.ids]
+words$plural.num <- 0
+words$plural.num[sing.ids] <- words$skaicius[plur.ids]
+
+output <- words[-plur.ids, -2]
 output <- output[order(output[, 2], decreasing=TRUE), ]
 write.csv(output, file="burton - plurals removed.csv", row.names=FALSE)
